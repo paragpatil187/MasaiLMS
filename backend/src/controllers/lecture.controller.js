61df53ac8a87c412c88ed97e28a3340438b954fe
parag <paragpatil187@gmail.com>
2022-09-01 13:37:17 +0530

    backend  folder struturecomplete database connection remain

---
+++ backend/src/controllers/lecture.controller.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const Assignment = require("../model/assignment.model")
+
+const crudController = require("./crud.controller")
+
+const router = express.Router();
+
+router.post ("", crudController.post(Assignment))  
+router.get("", crudController.get(Assignment))
+ 
+
+module.exports = router;
