26c318e819e5acd7d661d2cc745e74bf81361d26
parag <paragpatil187@gmail.com>
2022-09-02 14:22:10 +0530

    connect with database

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect("")
+    return mongoose.connect("mongodb+srv://paragpatil187:Parag21@cluster0.0ljsu.mongodb.net/?retryWrites=true&w=majority")
 }
 
 module.exports = connect
