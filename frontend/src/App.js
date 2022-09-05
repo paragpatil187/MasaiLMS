2129841acade4f2e7877ac4874a48ed8f3deb8f0
parag <paragpatil187@gmail.com>
2022-09-05 13:34:58 +0530

    homepage with default lecture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Lectures from './components/Lectures';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
 
       <Routes>
+      <Route path='/' element={<Home />} /> 
       <Route path='/lectures' element={<Lectures />} /> 
 
       </Routes>
