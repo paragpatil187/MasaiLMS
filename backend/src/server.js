0d775ec7cf3119d385db8bce7bf7ea2919212340
parag <paragpatil187@gmail.com>
2022-09-10 21:55:36 +0530

    backend code rectified

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+const cors=require("cors")
+
+
 const express = require("express");
 
 const connect = require("./configs/db")
@@ -7,14 +11,16 @@ const lectureController = require("./controllers/lecture.controller");
 const assignmentController = require("./controllers/assignment.controller"); 
 
 const app = express();
+dotenv.config();
 
+app.use(cors())
 app.use(express.json());  
 
 app.use("/lecture", lectureController);
 app.use("/assignment", assignmentController);
 
-const port  =  3001
-app.listen(port, async function(){
+const PORT  = process.env.PORT || 3005;
+app.listen(PORT, async function(){
     await connect();
-    console.log(`listening on port ${port}`)
+    console.log(`listening on port ${PORT}`)
 });
